0552e0ab0f09d44290e3e05c779f097de0d0d655
sksksk2024 <157216629+sksksk2024@users.noreply.github.com>
2024-08-07 19:26:32 +0100

    Added the project

---
+++ dist/script.js
@@ -0,0 +1,1 @@
+"use strict";console.log("hello world");//# sourceMappingURL=script.js.map
---
+++ gulpfile.js
@@ -0,0 +1,124 @@
+// // Initialize modules
+// const { src, dest, watch, series } = require('gulp');
+// const sass = require('gulp-sass')(require('sass'));
+// const postcss = require('gulp-postcss');
+// const autoprefixer = require('autoprefixer');
+// const cssnano = require('cssnano');
+// const babel = require('gulp-babel');
+// const terser = require('gulp-terser');
+// const browsersync = require('browser-sync').create();
+
+// // Sass Task
+// function scssTask() {
+//   return src('app/scss/style.scss', { sourcemaps: true })
+//     .pipe(sass().on('error', sass.logError))
+//     .pipe(postcss([autoprefixer(), cssnano()]))
+//     .pipe(dest('dist', { sourcemaps: '.' }))
+//     .pipe(browsersync.stream());
+// }
+
+// // JavaScript Task
+// function jsTask() {
+//   return src('app/js/script.js', { sourcemaps: true })
+//     .pipe(babel({ presets: ['@babel/preset-env'] }))
+//     .pipe(terser())
+//     .pipe(dest('dist', { sourcemaps: '.' }))
+//     .pipe(browsersync.stream());
+// }
+
+// // Browsersync
+// function browserSyncServe(cb) {
+//   browsersync.init({
+//     server: {
+//       baseDir: '.',
+//     },
+//     notify: {
+//       styles: {
+//         top: 'auto',
+//         bottom: '0',
+//       },
+//     },
+//   });
+//   cb();
+// }
+
+// function browserSyncReload(cb) {
+//   browsersync.reload();
+//   cb();
+// }
+
+// // Watch Task
+// function watchTask() {
+//   watch('*.html', browserSyncReload);
+//   watch(['app/scss/**/*.scss', 'app/js/**/*.js'], series(scssTask, jsTask, browserSyncReload));
+// }
+
+// // Default Gulp Task
+// exports.default = series(scssTask, jsTask, browserSyncServe, watchTask);
+
+// // Build Gulp Task
+// exports.build = series(scssTask, jsTask);
+
+const { src, dest, watch, series } = require('gulp');
+const sass = require('gulp-sass')(require('sass'));
+const postcss = require('gulp-postcss');
+const autoprefixer = require('autoprefixer');
+const cssnano = require('cssnano');
+const babel = require('gulp-babel');
+const terser = require('gulp-terser');
+const browsersync = require('browser-sync').create();
+
+// Sass Task
+function scssTask() {
+  console.log('Running scssTask...');
+  return src('app/scss/style.scss', { sourcemaps: true })
+    .pipe(sass().on('error', sass.logError))
+    .pipe(postcss([autoprefixer(), cssnano()]))
+    .pipe(dest('dist', { sourcemaps: '.' }))
+    .pipe(browsersync.stream())
+    .on('end', () => console.log('scssTask complete.'));
+}
+
+// JavaScript Task
+function jsTask() {
+  console.log('Running jsTask...');
+  return src('app/js/script.js', { sourcemaps: true })
+    .pipe(babel({ presets: ['@babel/preset-env'] }))
+    .pipe(terser())
+    .pipe(dest('dist', { sourcemaps: '.' }))
+    .pipe(browsersync.stream())
+    .on('end', () => console.log('jsTask complete.'));
+}
+
+// Browsersync
+function browserSyncServe(cb) {
+  browsersync.init({
+    server: {
+      baseDir: '.',
+    },
+    notify: {
+      styles: {
+        top: 'auto',
+        bottom: '0',
+      },
+    },
+  });
+  cb();
+}
+
+function browserSyncReload(cb) {
+  browsersync.reload();
+  cb();
+}
+
+// Watch Task
+function watchTask() {
+  watch('*.html', browserSyncReload);
+  watch(['app/scss/**/*.scss', 'app/js/**/*.js'], series(scssTask, jsTask, browserSyncReload));
+}
+
+// Default Gulp Task
+exports.default = series(scssTask, jsTask, browserSyncServe, watchTask);
+
+// Build Gulp Task
+exports.build = series(scssTask, jsTask);
